3a1cf9a1b0e858a2ea5a5ca72379898387bff808
Kareem Tarek Seif <kareemtseif@gmail.com>
2024-10-30 02:01:36 +0300

    create and provide view notes cubit

--- lib/cubits/view_notes/view_notes_cubit.dart
+++ lib/cubits/view_notes/view_notes_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constant.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'view_notes_state.dart';
 
 class ViewNotesCubit extends Cubit<ViewNotesState> {
   ViewNotesCubit() : super(ViewNotesInitial());
+
+  viewNotes() {
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      var notes = notesBox.values.toList();
+      emit(ViewNotesSuccess(notes));
+    } catch (e) {
+      emit(ViewNotesFailure(e.toString()));
+    }
+  }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/view_notes/view_notes_cubit.dart';
 import 'package:notes_app/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/notes_view_body.dart';
@@ -8,24 +10,27 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            context: context,
-            builder: (context) {
-              return const AddNoteBottomSheet();
-            },
-          );
-        },
-        child: const Icon(Icons.add),
+    return BlocProvider(
+      create: (context) => ViewNotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheet();
+              },
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
+        appBar: customAppBar(icon: Icons.search, title: 'Notes'),
+        body: const NotesViewBody(),
       ),
-      appBar: customAppBar(icon: Icons.search, title: 'Notes'),
-      body: const NotesViewBody(),
     );
   }
 }
